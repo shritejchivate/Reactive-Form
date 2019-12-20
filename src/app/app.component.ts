2f2fdbf34a03a81f69e3aafda3154d1783dab174
Shritej Chivate <55456378+shritejchivate@users.noreply.github.com>
2019-12-20 04:07:44 -0800

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'reactiveform';
+
+  form = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6)
+    ])
+    
+   });
+    constructor() { }
+  
+    get firstname(){
+      return this.form.get('firstName')
+    }
+    ngOnInit() {
+    }
+  
+    onSubmit(){
+     alert(JSON.stringify(this.form.value));
+    }
+
+}
+
